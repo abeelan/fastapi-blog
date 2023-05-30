2576334a76f94577cb10769d60bacd6e6893cb9e
lan <lanzy.nice@gmail.com>
2023-05-31 00:45:20 +0800

    feat: Add server cmdline.

--- src/blog/cmdline.py
+++ src/blog/cmdline.py
@@ -1,3 +1,15 @@
+"""
+命令行工具定义
+"""
+
+import click
+
+from .config import settings
+from .server import Server
+
+__version__ = "1.0"
+
+
 @click.group(invoke_without_command=True)
 @click.pass_context
 @click.option('-V', '--version', is_flag=True, help='Show version and exit.')
@@ -6,3 +18,21 @@ def main(ctx, version):
         click.echo(__version__)
     elif ctx.invoked_subcommand is None:
         click.echo(ctx.get_help())
+
+
+@main.command()
+@click.option('-h', '--host', show_default=True, help=f'Host IP. Default: {settings.HOST}')
+@click.option('-p', '--port', show_default=True, type=int, help=f'Port. Default: {settings.PORT}')
+@click.option('--level', help='Log level')
+def server(host, port, level):
+    """Start server."""
+    kwargs = {
+        'LOGLEVEL': level,
+        'HOST': host,
+        'PORT': port,
+    }
+    for name, value in kwargs.items():
+        if value:
+            settings.set(name, value)
+
+    Server().run()
